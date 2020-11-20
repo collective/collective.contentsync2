690afcd6c0c5feb3edda7c0ec3a9d655fc96b840
Andreas Jung <info@zopyx.com>
2020-11-20 13:39:18 +0100

    updated

--- src/collective/contentsync/sync/controlpanel.py
+++ src/collective/contentsync/sync/controlpanel.py
@@ -2,8 +2,8 @@
 """Sync controlpanel view."""
 
 from collective.contentsync import _
-from collective.contentsync.sync.sync import full_sync, sync_queue
-from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
+from collective.contentsync.sync.sync import full_sync
+from collective.contentsync.sync.sync import sync_queue
 from plone.app.registry.browser import controlpanel
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.autoform import directives
@@ -12,16 +12,14 @@ from plone.z3cform import layout
 from z3c.form import button
 from z3c.form.browser.multi import MultiWidget
 from zope import schema
-from zope.interface import implementer, Interface
+from zope.interface import implementer
+from zope.interface import Interface
 
 import plone.api
 
 
-DEFAULT_OMITTED_UPDATE_FIELDS = []
-
-
-class ITargetRow(model.Schema):
-    target = schema.TextLine(title="Sync target", default='', required=False)
+DEFAULT_OMITTED_UPDATE_FIELDS = [
+]
 
 
 class ISyncControlPanelForm(Interface):
@@ -34,7 +32,8 @@ class ISyncSettings(model.Schema):
     sync_immediately = schema.Bool(
         description=_(
             u"If activated, a synchronization will be tried right away. If it fails, "
-            u"the content item will be added to the queue.", ),
+            u"the content item will be added to the queue.",
+        ),
         default=False,
         required=False,
         title=_(u"Sync immediately"),
@@ -44,33 +43,33 @@ class ISyncSettings(model.Schema):
         default=DEFAULT_OMITTED_UPDATE_FIELDS,
         description=_(
             u"This list contains field names which should be ignored when the remote "
-            u"item already exists. Add one item per line."),
+            u"item already exists. Add one item per line."
+        ),
         required=False,
         title=_(u"Ignored fields for update"),
         value_type=schema.TextLine(),
     )
 
-    #    directives.widget("targets", MultiWidget)
-    #    directives.widget(targets=DataGridFieldFactory)
+    directives.widget("targets", MultiWidget)
     targets = schema.List(
         description=_(u"Synchronization targets"),
         required=False,
         title=_(u"Targets"),
-        value_type=schema.TextLine(title="foo")
-        #        value_type=DictRow(
-        #            description=_(
-        #               u"Please specify targets in the form of “key|title|url|username|password”."
-        #            ),
-        #            title=_(u"Target definition"),
-        #            schema=ITargetRow
-        #        )
+        default=['abc'],
+        value_type=schema.TextLine(
+            description=_(
+                u"Please specify targets in the form of “key|title|url|username|password”."
+            ),
+            title=_(u"Target definition"),
+        ),
     )
 
     directives.mode(ISyncControlPanelForm, queue="display")
     queue = schema.Set(
         description=_(
             u"A list of content items which should be synced with next "
-            u"synchronization run."),
+            u"synchronization run."
+        ),
         required=False,
         title=_(u"Sync Queue"),
         value_type=schema.TextLine(title=_(u"Path")),
@@ -83,7 +82,7 @@ class SyncControlPanelForm(controlpanel.RegistryEditForm):
 
     schema = ISyncSettings
     schema_prefix = "collective.contentsync"
-    label = _(u"Plone Sync Settings")
+    label = _(u"Content Sync Settings")
     buttons = controlpanel.RegistryEditForm.buttons.copy()
     handlers = controlpanel.RegistryEditForm.handlers.copy()
 
@@ -102,23 +101,22 @@ class SyncControlPanelForm(controlpanel.RegistryEditForm):
             message=_(u"Synchronization run was successful."),
             request=self.request,
         )
-        self.request.response.redirect(u"{0}/{1}".format(
-            plone.api.portal.get().absolute_url(),
-            "@@collective.contentsync-settings"))
+        self.request.response.redirect(
+            u"{0}/{1}".format(
+                plone.api.portal.get().absolute_url(), "@@collective.contentsync-settings"
+            )
+        )
 
     @button.buttonAndHandler(
         _(u"Full sync"),
         name="full_sync",
     )
     def handle_full_sync(self, action):
-        queue = plone.api.portal.get_registry_record(
-            "collective.contentsync.queue")
+        queue = plone.api.portal.get_registry_record("collective.contentsync.queue")
         queue = queue or set()
         missed = full_sync()
         queue = queue & missed
-        plone.api.portal.set_registry_record("collective.contentsync.queue",
-                                             queue)
+        plone.api.portal.set_registry_record("collective.contentsync.queue", queue)
 
 
-SyncControlPanelView = layout.wrap_form(SyncControlPanelForm,
-                                        ControlPanelFormWrapper)
+SyncControlPanelView = layout.wrap_form(SyncControlPanelForm, ControlPanelFormWrapper)
